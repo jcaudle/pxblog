947a644059f7d43789222cd9349465f478b0610d
Joseph Caudle <joseph@josephcaudle.com>
2016-12-22 20:10:32 -0600

    Associate posts with users

--- web/models/user.ex
+++ web/models/user.ex
@@ -12,6 +12,8 @@ defmodule Pxblog.User do
     # Virtual Fields
     field :password, :string, virtual: true
     field :password_confirmation, :string, virtual: true
+
+    has_many :posts, Pxblog.Post
   end
 
   @doc """
