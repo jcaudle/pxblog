84291aa60dae4eb27e2fd5fdf4a2cbefc7392068
Joseph Caudle <joseph@josephcaudle.com>
2016-12-27 00:55:52 -0600

    Enforce authorization

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -4,6 +4,7 @@ defmodule Pxblog.PostController do
   alias Pxblog.Post
 
   plug :assign_user
+  plug :authorize_user when action in [:new, :create, :update, :edit, :delete]
 
   def index(conn, _params) do
     posts = Repo.all(assoc(conn.assigns[:user], :posts))
@@ -87,5 +88,17 @@ defmodule Pxblog.PostController do
     |> put_flash(:error, "Invalid user!")
     |> redirect(to: page_path(conn, :index))
     |> halt
+  end
+
+  defp authorize_user(conn, _opts) do
+    user = get_session(conn, :current_user)
+    if user && Integer.to_string(user.id) == conn.params["user_id"] do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not authorized to modify that post!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
